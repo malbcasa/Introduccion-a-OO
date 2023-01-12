83e3f1cd3f53f40a1f33092227b88322be5b0957
Mauricio <mauriciocasarottocad@outlook.com.ar>
2023-01-12 02:21:21 -0300

    Aula 02 completa

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 class Cliente {
-    nombreCliente;
+    nombreCliente;  
     dniCliente;
-    numeroCliente;
-    saldoCuenta;
+    rutCliente;
+}
+
+class CuentaCorriente {
+    numero;
+    #saldo;
+    agencia;
+
+    constructor(){
+        this.#saldo = 0;
+        this.numero = '';
+        this.agencia = '';
+    }
+
+    depositoEnCuenta(valor){
+        if(valor > 0){
+            this.#saldo += valor;
+            
+        }
+    }
+
+    retirarDeCuenta(valor){
+        if (valor <= this.#saldo){
+            this.#saldo -= valor;
+        }
+    }
+
+    verSaldo() {
+        return this.#saldo;
+    }
 }
